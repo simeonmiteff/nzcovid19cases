00e0ee1c7c6849030316e74eede856050d45698d
Simeon Miteff <simeon.miteff@gmail.com>
2020-03-28 15:01:20 +1300

    JIT scraping per command

--- cmd/nzcovid19-cli/main.go
+++ cmd/nzcovid19-cli/main.go
@@ -53,43 +53,49 @@ func main() {
 		usage()
 	}
 
-	rawCases, err := nzcovid19cases.ScrapeCases()
-	if err != nil {
-		abort(err, 2)
-	}
-
-	caseStats, err := nzcovid19cases.ScrapeCaseStats()
-	if err != nil {
-		abort(err, 3)
-	}
-
-	normCases, err := nzcovid19cases.NormaliseCases(rawCases)
-	if err != nil {
-		abort(err, 4)
-	}
-
-	levelInt, levelString, err := nzcovid19cases.ScrapeLevel()
-	if err != nil {
-		abort(err, 5)
-	}
-
-	gS, gR, err := nzcovid19cases.ScrapeGrants()
-	if err != nil {
-		abort(err, 6)
-	}
 
 	var result string
+	var err error
 	switch dataType {
 	case "cases":
+		rawCases, err := nzcovid19cases.ScrapeCases()
+		if err != nil {
+			abort(err, 2)
+		}
+		normCases, err := nzcovid19cases.NormaliseCases(rawCases)
+		if err != nil {
+			abort(err, 4)
+		}
 		result, err = nzcovid19cases.RenderCases(normCases, viewType)
 	case "locations":
+		// FIXME: dedup code
+		rawCases, err := nzcovid19cases.ScrapeCases()
+		if err != nil {
+			abort(err, 2)
+		}
+		normCases, err := nzcovid19cases.NormaliseCases(rawCases)
+		if err != nil {
+			abort(err, 4)
+		}
 		locations := nzcovid19cases.BuildLocations(normCases)
 		result, err = nzcovid19cases.RenderLocations(locations, viewType)
 	case "alertlevel":
+		levelInt, levelString, err := nzcovid19cases.ScrapeLevel()
+		if err != nil {
+			abort(err, 5)
+		}
 		result, err = nzcovid19cases.RenderLevels(levelInt, levelString, viewType)
 	case "grants":
+		gS, gR, err := nzcovid19cases.ScrapeGrants()
+		if err != nil {
+			abort(err, 6)
+		}
 		result, err = nzcovid19cases.RenderGrants(gS, gR, viewType)
 	case "casestats":
+		caseStats, err := nzcovid19cases.ScrapeCaseStats()
+		if err != nil {
+			abort(err, 3)
+		}
 		result, err = nzcovid19cases.RenderCaseStats(caseStats, "json")
 	}
 
