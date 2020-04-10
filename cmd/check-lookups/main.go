efb5c35b9476a8ea84d029c2567caa961d479fae
Simeon Miteff <simeon.miteff@gmail.com>
2020-04-10 22:06:07 +1200

    Style

--- cmd/check-lookups/main.go
+++ cmd/check-lookups/main.go
@@ -9,27 +9,34 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	caseStats, err := nzcovid19cases.ScrapeCaseStats()
 	if err != nil {
 		panic(err)
 	}
+
 	_, err = nzcovid19cases.RenderCaseStats(caseStats, "json")
 	if err != nil {
 		panic(err)
 	}
+
 	normCases, err := nzcovid19cases.NormaliseCases(rawCases)
 	if err != nil {
 		panic(err)
 	}
+
 	_, err = nzcovid19cases.RenderCases(normCases, "csv")
 	if err != nil {
 		panic(err)
 	}
+
 	locations := nzcovid19cases.BuildLocations(normCases)
+
 	_, err = nzcovid19cases.RenderLocations(locations, "json")
 	if err != nil {
 		panic(err)
 	}
+
 	levelInt, levelString, err := nzcovid19cases.ScrapeLevel()
 	if err != nil {
 		panic(err)
@@ -38,18 +45,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	gS, gR, err := nzcovid19cases.ScrapeGrants()
 	if err != nil {
 		panic(err)
 	}
+
 	_, err = nzcovid19cases.RenderGrants(gS, gR, "json")
 	if err != nil {
 		panic(err)
 	}
+
 	clusters, err := nzcovid19cases.ScrapeClusters()
 	if err != nil {
 		panic(err)
 	}
+
 	_, err = nzcovid19cases.RenderClusters(clusters, "json")
 
 }
